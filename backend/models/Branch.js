2d3fadc8cba6cd156d8e9dd4138eeeae1fcfd40d
Mujtaba Ahmad <mma212005@gmail.com>
2026-05-10 23:22:55 +0500

    New backend whole code

--- backend/models/Branch.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const branchSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }, // e.g., 'Islamabad', 'Lahore', 'Karachi', 'Remote' [cite: 120-125]
-  address: { type: String },
-  contactEmail: { type: String }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Branch', branchSchema);
