e49a27425871bbf10d7b439fcb6e1e5dc0ebe9d1
Mujtaba Ahmad <mma212005@gmail.com>
2026-05-11 19:36:49 +0500

    Add User model with schema definition

---
+++ backend/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['candidate', 'hr', 'admin'], default: 'candidate' }
+}, { timestamps: true }); // Automatically adds createdAt and updatedAt dates
+
+module.exports = mongoose.model('User', userSchema);
